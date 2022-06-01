33a599b65f3757b248081ba82014cd9bab4059f6
Antonino Esposito <antonino.esposito@infocube.it>
2022-06-01 09:40:12 +0200

    Add secondary server cleanup

---
+++ mktg_backupdomain.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DOMAINNAME=''
+TMPSTORAGE='/root/backup/mailboxes/migrated'
+S3STORAGE='emailold'
+
+# Commands.
+CMD_DATE='/bin/date'
+CMD_COMPRESS='/bin/tar -czvf'
+CMD_B2='/root/b2 upload-file '
+
+# Date.
+YEAR="$(${CMD_DATE} +%Y)"
+MONTH="$(${CMD_DATE} +%m)"
+DAY="$(${CMD_DATE} +%d)"
+TIME="$(${CMD_DATE} +%H.%M.%S)"
+TIMESTAMP="${YEAR}-${MONTH}-${DAY}"
+
+#
+#	Prepare temp storage
+#
+if [ -d "$TMPSTORAGE" ]
+then
+    echo "Temporary storage directory exists."
+    # rm -Rf $TMPSTORAGE/*.tar.gz
+else
+    echo "Error: Temporary directory does not exists."
+    mkdir -p $TMPSTORAGE
+fi
+
+
+#
+#	Check the remote storage - NFS/ZFS
+#
+if [ -d "/var/vmail/$DOMAINNAME" ] 
+then
+    echo "Remote directory exists." 
+    ${CMD_COMPRESS} $TMPSTORAGE/${TIMESTAMP}_${DOMAINNAME}.tar.gz /var/vmail/$DOMAINNAME
+
+    # Store the archive to BackBlaze bucket
+    ${CMD_B2} ${S3STORAGE} $TMPSTORAGE/${TIMESTAMP}_${DOMAINNAME}.tar.gz  ${TIMESTAMP}_${DOMAINNAME}.tar.gz 
+
+else
+    echo "Error: Remote directory does not exists."
+fi
